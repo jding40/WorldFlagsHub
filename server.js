84c1e92bd170ac640b6ea1cf64de2a16d5a79761
Coca4017 <jding40@myseneca.ca>
2024-10-08 11:31:07 -0400

    update 500.ejs

--- server.js
+++ server.js
@@ -27,8 +27,8 @@ app.use(express.json());  // 用于解析 JSON 格式的请求体
 app.use(clientSessions({
   cookieName: "session", // this is the object name that will be added to 'req'
   secret: "freshProInventoryManageSystemDesignedByJianzhongDing", // this should be a long un-guessable string.
-  duration: 10 * 60 * 1000, // duration of the session in milliseconds (10 minutes)
-  activeDuration: 1000 * 60 // the session will be extended by this many ms each request (1 minute)
+  duration: 180 * 60 * 1000, // duration of the session in milliseconds (3 hours)
+  activeDuration: 1000 * 60 * 30 // the session will be extended by this many ms each request (30 minute)
 }));
 
 
